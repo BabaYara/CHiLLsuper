773f4ed135c77866cac9320a0457ff47e44d8e3a
Tharindu Rusira <tharindurusira@gmail.com>
2016-11-09 02:47:45 -0700

    integer constraint solver implemented

--- csolver.py
+++ csolver.py
@@ -3,10 +3,14 @@ Integer Constraint Solver to generate
 actual (and valid) CHiLL script instances
 '''
 from constraint import *
- 
+import re
+
+varnames = [] # a list of variable names for fast access
 params= [] # list of 3-tuples, (name, type, [domain])
 knowns = [] # list of constraints
-xforms = [] #list of xforms in order, 
+
+XFORMS = ['tile', 'distribute', 'skew', 'fuse', 'permute', 'omp_par_for', 'partial_sums'] #list of xforms in order, 
+
 problem = Problem()
 
 class Param:
@@ -20,6 +24,8 @@ def generate_parameter_domain(superscript):
 
     file = open(superscript,'r')
     lines = file.readlines()
+    file.close()
+
     nlines = len(lines)
     lno = 0
     while ('@start_param_region' not in lines[lno]):
@@ -54,16 +60,20 @@ def generate_parameter_domain(superscript):
     
             p = Param()
             p.name = args[0]
+            varnames.append(p.name)
             p.type = args[1]
             p.domain = []
             for val in args[2:]:
-                p.domain.append(val)
+                p.domain.append(int(val))
 
             #print p.name, p.type, p.domain
 
             params.append(p)           
+            if p.type == 'enum':
+                problem.addVariable(p.name,p.domain)
             
-        
+            #TODO:add support to other types
+
         elif(lines[lno].startswith('known')):
             '''
             known line looks like the following
@@ -76,18 +86,45 @@ def generate_parameter_domain(superscript):
         
             for c in cons: 
                 knowns.append(c)
+                # find what variables are in the constraint
+                regex = '\+|-|\*|/|==|<=|>=|<|>| '
+                terms_in_c = re.split(regex,c)
 
+                if '' in terms_in_c:
+                    continue
+                #now filter out the variables among the terms
+                v = []
+                lambda_str = 'lambda '
 
-        elif(lines[lno].startswith('')):
+                for term in terms_in_c:
+                    if term in varnames:
+                        v.append(term)
+                        lambda_str += term+','
+
+                lambda_str = lambda_str[:-1] #remove last ,
+                
+                lambda_str += ': ('+c+')'
+                var_tuple = tuple(v)
+
+                exec('lambda_function ='+lambda_str)
+
+                #now add the constraint to the problem
+                problem.addConstraint(lambda_function, var_tuple)
+
+             
+        else:
+            '''
+            we expect everything else to represent a parameterized xform. 
+            If something's wrong, CHiLL shall catch it
+            '''
             pass
 
         lno += 1
-        
+
+    #solve the constraint problem
+    solutions = problem.getSolutions()
+    print solutions
 
     while(lno<nlines):
         suffix += lines[lno]
         lno += 1
-    
-    file.close()
-
-
